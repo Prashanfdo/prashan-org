2e59973551247b3c05ee0aa7eea0d3874c15ba01
Prashan Fernando <prashanfdo@gmail.com>
2022-02-06 16:35:32 +0530

    updated stories

--- web/src/pages/blog/[slug]/index.tsx
+++ web/src/pages/blog/[slug]/index.tsx
@@ -34,7 +34,7 @@ function PortfolioSinglePage({ post }: PortfolioSinglePageProps) {
         <div className="flex justify-between w-full items-center md:flex-row flex-col mb-0">
           <h1 className="text-2xl flex-none font-bold font-display">{title}</h1>
         </div>
-        <div className="text-sm font-semibold text-gray-400 mb-4">· {format(parseISO(date), 'MMMM do, uuu')}</div>
+        <div className="text-sm font-semibold text-gray-400 mb-4">{format(parseISO(date), 'MMMM do, uuu')}</div>
         <div className="w-full object-cover rounded overflow-hidden shadow-xl border relative h-96">
           <Image src={image} alt={image} layout="fill" objectFit="cover" />
         </div>
@@ -48,26 +48,26 @@ PortfolioSinglePage.layout = MainLayout;
 
 export default PortfolioSinglePage;
 
-export async function getStaticProps(context: { params: Pick<BlogPost, 'slug'> }) {
-  const { params } = context;
+// export async function getStaticProps(context: { params: Pick<BlogPost, 'slug'> }) {
+//   const { params } = context;
 
-  const allPosts = getAllPosts();
+//   const allPosts = getAllPosts();
 
-  const post = allPosts.find((i) => i.slug === params.slug);
-  if (post) {
-    post.content = (await renderToString(post.content)) as unknown as string;
-  }
+//   const post = allPosts.find((i) => i.slug === params.slug);
+//   if (post) {
+//     post.content = (await renderToString(post.content)) as unknown as string;
+//   }
 
-  return {
-    props: {
-      post,
-    },
-  };
-}
+//   return {
+//     props: {
+//       post,
+//     },
+//   };
+// }
 
-export async function getStaticPaths() {
-  const allPosts = getAllPosts();
+// export async function getStaticPaths() {
+//   const allPosts = getAllPosts();
 
-  const paths = allPosts.map((post) => ({ params: { slug: post.slug } }));
-  return { paths, fallback: false };
-}
+//   const paths = allPosts.map((post) => ({ params: { slug: post.slug } }));
+//   return { paths, fallback: false };
+// }
--- web/src/pages/portfolio/index.tsx
+++ web/src/pages/portfolio/index.tsx
@@ -1,6 +1,7 @@
 import MainLayout from 'components/layouts/MainLayout';
 import portfolioData from 'data/portfolio/data';
 import Link from 'next/link';
+import Image from 'next/image';
 
 function PortfolioPage() {
   return (
@@ -8,12 +9,15 @@ function PortfolioPage() {
       <div className="flex justify-between w-full items-center md:flex-row flex-col">
         <h1 className="text-2xl flex-none font-bold font-display">Portfolio</h1>
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-8 mt-12 pb-16">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-8 mt-12 md:pb-16 pb-32">
         {portfolioData.map(([title, images], index) => (
           <Link passHref href={`/portfolio/${index + 1}`} key={title}>
             <a className="w-full h-full block transition-all hover:scale-110 active:scale-105 duration-200">
               <div className="h-full rounded overflow-hidden shadow-2xl">
-                <img className="w-full h-60 lg:h-80" src={`/assets/images/portfolio/${images.split(',')[0]}`} alt={title} />
+                <div className="w-full h-52 lg:h-60 relative">
+                  <Image objectFit="cover" src={`/assets/images/portfolio/${images.split(',')[0]}`} alt={title} layout="fill" />
+                </div>
+                {/* <img className="w-full h-60 lg:h-80" src={`/assets/images/portfolio/${images.split(',')[0]}`} alt={title} /> */}
                 <div className="px-6 pt-4 pb-3">
                   <div className="font-bold text-xl mb-2">{title}</div>
                 </div>
